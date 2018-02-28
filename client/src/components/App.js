8ef4f8b4f623178a5bde1c39e477e9bfda703255
Kris Gray <kris.m.gray@gmail.com>
2018-02-28 12:33:51 -0700

    but in Gallery, need to fix

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import NoMatch from './NoMatch';
 import NavBar from './NavBar';
 import Login from './Login';
@@ -6,7 +7,6 @@ import Flash from './Flash';
 import Home from './Home';
 import AboutUs from './AboutUs';
 import Register from './Register';
-import Gallery from './Gallery';
 import ContactUs from './ContactUs';
 import Map from './Map';
 import Calendar from './Calendar';
@@ -23,7 +23,6 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <Route exact path='/Register' component={Register} />
             <Route exact path='/AboutUs' component={AboutUs} />
-            <Route exact path='/Gallery' component={Gallery} />
             <Route exact path='/Calendar' component={Calendar} />
             <Route exact path='/ContactUs' component={ContactUs} />
             <Route component={NoMatch} />
